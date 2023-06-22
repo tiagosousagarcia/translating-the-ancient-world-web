c283de789a117ac0f3d1a198ab87980d3668087f
tiagosousagarcia <tiagosousagarcia@gmail.com>
2023-06-22 18:17:48 +0100

    basic data loading, disable js type checking

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,12 @@
+<script>
+    export let data
+
+    console.log(data.dataset)
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+{#each data.dataset as entry }
+<p>{entry.id}-{entry['Normalised Title']}-{entry.Date.getFullYear()}</p>    
+{/each}
