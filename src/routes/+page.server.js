18daf2a761db1b2082a1e375589469c991a8662a
tiagosousagarcia <tiagosousagarcia@gmail.com>
2023-06-26 00:59:33 +0100

    loading of data straght from the live spreadsheet, and basic filtering based on publication date

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -2,7 +2,7 @@ import {base} from '$app/paths'
 import {csvParse} from 'd3'
 
 export async function load({fetch}) {
-    let csvString = await fetch(`${base}/Dataset.csv`).then(function(response) {
+    let csvString = await fetch(`https://docs.google.com/spreadsheets/d/1puV33xy5x7-5ifiV6iYLIfwY2cRWEe9ms3R-yiMyv60/export?format=csv&gid=1304251320`).then(function(response) {
         if (response.ok) {
             return response.text();
         }
@@ -15,5 +15,13 @@ export async function load({fetch}) {
             'Translator': d['Translator'].split(' ; ')
         }
     });
-    return {dataset: datasetJson}
+    // example of filtering dataset
+    // let special = datasetJson.filter(entry => entry.author === 'Xenophon')
+
+    // get list of all possible years
+    let years = datasetJson.map(entry => entry['Date'].getFullYear());
+    // removes NaN years
+    years = years.filter(function(entry) {return !isNaN(entry)});
+
+    return {dataset: datasetJson, years: {min: Math.min(...years), max: Math.max(...years)}}
 }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,15 @@
 <script>
     export let data
 
+    let yearsMin = data.years.min
+    let yearsMax = data.years.max
+
     console.log(data.dataset)
 </script>
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-
-{#each data.dataset as entry }
-<p>{entry.id}-{entry['Normalised Title']}-{entry.Date.getFullYear()}</p>    
+<input type="text" bind:value="{yearsMin}"> - <input type="text" bind:value="{yearsMax}">;
+{#each data.dataset.filter(entry => entry.Date.getFullYear() >= yearsMin && entry.Date.getFullYear() <= yearsMax) as entry }
+<p>{entry.Date.getFullYear()}-{entry.author}-{entry.Translator}-{entry.Title}-{entry.City}</p>    
 {/each}
