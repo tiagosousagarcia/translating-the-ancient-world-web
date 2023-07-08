385411c015357683f7a82ce4b6f2233dbfb57618
tiagosousagarcia <tiagosousagarcia@gmail.com>
2023-07-08 20:21:49 +0100

    added expandable tab and animation

--- src/lib/MultipleValueFilter.svelte
+++ src/lib/MultipleValueFilter.svelte
@@ -1,10 +1,13 @@
 <script>
     import { onMount } from 'svelte';
+    import { slide } from 'svelte/transition';
     export let listNames;
     export let filterName;
 
     let maxVisible = 5;
     let showLess = false;
+
+    let expanded = false;
 
     function showMore() {
         maxVisible = listNames.length;
@@ -16,6 +19,10 @@
         showLess = false;
     }
 
+    function expand() {
+        expanded = !expanded;
+    }
+
     onMount( () => {
         if (listNames.length < 5) {
             maxVisible = listNames.length
@@ -25,17 +32,23 @@
 </script>
 
 <div>
-    <h3>{filterName}</h3>
-        {#each listNames.sort().slice(0, maxVisible) as name}
-            <label for="{name}" class="flex space-x-2">
-                <input type="checkbox" id={name} name = {name} class='checkbox'>
-                <p>{name}</p>
-            </label>
-        {/each}
-        {#if maxVisible < listNames.length}
-            <button on:click={showMore} on:keypress class="btn-sm variant-filled">More...</button>
-        {/if}
-        {#if showLess}
-            <button class="btn-sm variant-filled" on:click={showLessItems} on:keypress>Less...</button>
+    <button class="btn" on:click={expand} on:keypress>
+        <h3>{filterName} {!expanded ? '+' : '-'}</h3>
+    </button>
+        {#if expanded}
+            <div class="list" transition:slide>
+                {#each listNames.sort().slice(0, maxVisible) as name}
+                    <label for="{name}" class="flex space-x-2" transition:slide>
+                        <input type="checkbox" id={name} name = {name} class='checkbox'>
+                        <p>{name}</p>
+                    </label>
+                {/each}
+                {#if maxVisible < listNames.length}
+                    <button on:click={showMore} on:keypress class="btn-sm variant-filled">More...</button>
+                {/if}
+                {#if showLess}
+                    <button class="btn-sm variant-filled" on:click={showLessItems} on:keypress>Less...</button>
+                {/if}
+            </div>
         {/if}
 </div>
